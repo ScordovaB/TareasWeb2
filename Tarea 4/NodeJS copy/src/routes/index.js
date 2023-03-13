da52045f09adf104b68acdfb918fce35329be43d
ScordovaB <98288513+ScordovaB@users.noreply.github.com>
2023-03-12 18:09:04 -0600

    Eliminacion de archivos no utilizados

--- Tarea 4/NodeJS copy/src/routes/index.js
+++ Tarea 4/NodeJS copy/src/routes/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const rutasTareas = require('./tareas');
-const rutasUsuarios = require('./usuarios');
+//const rutasTareas = require('./tareas');
+//const rutasUsuarios = require('./usuarios');
 const rutaNoticias = require('./noticias');
 const {auth}  = require('./../middlewares');
 
@@ -9,7 +9,7 @@ const {auth}  = require('./../middlewares');
 //router.use('',auth);
 router.use('',auth);
 router.use('/noticias',rutaNoticias);
-router.use('/tareas',rutasTareas);
-router.use('/usuarios',rutasUsuarios);
+//router.use('/tareas',rutasTareas);
+//router.use('/usuarios',rutasUsuarios);
 
 module.exports = router;
