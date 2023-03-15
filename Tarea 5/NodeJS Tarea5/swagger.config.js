fde56cc870cf04bb48463216885adaa0e4a8cd65
ScordovaB <98288513+ScordovaB@users.noreply.github.com>
2023-03-14 20:13:47 -0600

    Swagger config actualizado

--- Tarea 5/NodeJS Tarea5/swagger.config.js
+++ Tarea 5/NodeJS Tarea5/swagger.config.js
@@ -4,8 +4,8 @@ module.exports = {
     swaggerDefinition:{
         swagger: "2.0",
         info:{
-            "title": "API Clase NodeJS",
-            "description":"API ejemplo de proyecto NodeJS",
+            "title": "API TAREAS S.C.B",
+            "description":"API Tareas CRUD, Santiago Cordova B",
             "version":"1.0.0",
             "servers": ["http://localhost:"+port]
         }
